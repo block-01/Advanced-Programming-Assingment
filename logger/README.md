ad6027591579c14132a4e8b1a72bf64a138efaef
Thomas Wilks <t.wilks@my.bpp.com>
2026-02-12 09:46:18 +0000

    chore: fixed code readability and add basic docs for logger

---
+++ logger/README.md
@@ -0,0 +1,41 @@
+# Logger
+
+This python package is a basic logger for logging events to the terminal and saving them to a file.
+
+## Functions
+
+### Debug
+
+This logs debug messages to the terminal outputting them in purple.
+
+#### Example
+
+```python
+from logger import logger
+
+logger.debug("Debug logger example.")
+```
+
+### Warning
+
+This logs warning messages to the terminal outputting them in yellow.
+
+#### Example
+
+```python
+from logger import logger
+
+logger.warning("Warning logger example.")
+```
+
+### Exception
+
+This logs exception messages to the terminal outputting them in Red along with the exception that occurred.
+
+#### Example
+
+```python
+from logger import logger
+
+logger.exception("Exception logger example.")
+```
--- test_logger.py
+++ test_logger.py
@@ -3,21 +3,23 @@ import pytest
 
 
 class TestLogger:
-        def test_debug(self):
+    def test_debug(self):
+        logger.test.test_case_start(self, "Debug logger test")
+        try:
+            logger.debug("debug test")
+        except pytest.fail.Exception:
+            logger.test.test_case_fail("test_debug")
 
-                try:
-                        logger.debug("debug test")
-                except pytest.fail.Exception:
-                        logger.test.test_case_fail("test_debug")
+    def test_warning(self):
+        logger.test.test_case_start(self, "Warning logger test")
+        try:
+            logger.warning("warning test")
+        except pytest.fail.Exception:
+            logger.test.test_case_fail("test_warning")
 
-        def test_warning(self):
-                try:
-                        logger.warning("warning test")
-                except pytest.fail.Exception:
-                        logger.test.test_case_fail("test_warning")
-
-        def test_exception(self):
-                try:
-                        logger.exception("exception test")
-                except pytest.fail.Exception:
-                        logger.test.test_case_fail("test_exception")
+    def test_exception(self):
+        logger.test.test_case_start(self, "Exception logger test")
+        try:
+            logger.exception("exception test")
+        except pytest.fail.Exception:
+            logger.test.test_case_fail("test_exception")
