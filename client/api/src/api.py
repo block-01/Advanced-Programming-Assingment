12ed0ceec210e4fbd8928781f12e12a891ed2939
Thomas Wilks <t.wilks@my.bpp.com>
2026-02-19 13:42:09 +0000

    chore: improve return types and type hinting inside of the api

--- client/api/src/api.py
+++ client/api/src/api.py
@@ -6,14 +6,15 @@ on port 5000.
 
 from logger import logger
 from sys_info import sys_info
-from flask import Flask
+from flask import Flask, Response, request, jsonify
 
 
 app = Flask(__name__)
+app.debug = True  # TODO: Disable once development is complete.
 
 
-@app.route("/api/info/full")
-def _system_info_full() -> dict[str, str | int]:
+@app.route("/api/info/full", methods=["GET"])
+def _system_info_full() -> tuple[Response, int]:
     """Full system info
 
     Request type: GET
@@ -23,24 +24,30 @@ def _system_info_full() -> dict[str, str | int]:
     Return:
         Information about the systems OS and hardware.
     """
+
     logger.api("/api/info/full", logger.request_type.GET)
-    return {
-        "os_version": sys_info.software.os.version,
-        "os_hostname": sys_info.software.os.hostname,
-        "os_shell": sys_info.software.os.shell,
-        "net_ip": sys_info.software.network.net_ip,
-        "net_mac_address": sys_info.software.network.net_mac_address,
-        "os_cpu_arch": sys_info.sys_hardware.cpu.cpu_arch,
-        "os_cpu_cores": sys_info.sys_hardware.cpu.cpu_cores,
-        "os_cpu_core_clock_max": sys_info.sys_hardware.cpu.cpu_core_clock_max,
-        "os_cpu_core_clock_min": sys_info.sys_hardware.cpu.cpu_core_clock_min,
-        "os_cpu_threads": sys_info.sys_hardware.cpu.cpu_threads,
-        "os_hard_ram": sys_info.sys_hardware.ram.size,
-    }
+    return (
+        jsonify(
+            {
+                "os_version": sys_info.software.os.version,
+                "os_hostname": sys_info.software.os.hostname,
+                "os_shell": sys_info.software.os.shell,
+                "net_ip": sys_info.software.network.net_ip,
+                "net_mac_address": sys_info.software.network.net_mac_address,
+                "os_cpu_arch": sys_info.sys_hardware.cpu.cpu_arch,
+                "os_cpu_cores": sys_info.sys_hardware.cpu.cpu_cores,
+                "os_cpu_core_clock_max": sys_info.sys_hardware.cpu.cpu_core_clock_max,
+                "os_cpu_core_clock_min": sys_info.sys_hardware.cpu.cpu_core_clock_min,
+                "os_cpu_threads": sys_info.sys_hardware.cpu.cpu_threads,
+                "os_hard_ram": sys_info.sys_hardware.ram.size,
+            }
+        ),
+        200,
+    )
 
 
-@app.route("/api/info/os")
-def _system_info_os() -> dict[str, str]:
+@app.route("/api/info/os", methods=["GET"])
+def _system_info_os() -> tuple[Response, int]:
     """OS info
 
     Request type: GET
@@ -50,19 +57,25 @@ def _system_info_os() -> dict[str, str]:
     Return:
         Information about the OS running on the server.
     """
+
     logger.api("/api/info/os", logger.request_type.GET)
-    return {
-        "os_platform": sys_info.software.os.hostname,
-        "os_version": sys_info.software.os.version,
-        "os_hostname": sys_info.software.os.hostname,
-        "os_shell": sys_info.software.os.shell,
-        "net_ip": sys_info.software.network.net_ip,
-        "net_mac_address": sys_info.software.network.net_mac_address,
-    }
+    return (
+        jsonify(
+            {
+                "os_platform": sys_info.software.os.hostname,
+                "os_version": sys_info.software.os.version,
+                "os_hostname": sys_info.software.os.hostname,
+                "os_shell": sys_info.software.os.shell,
+                "net_ip": sys_info.software.network.net_ip,
+                "net_mac_address": sys_info.software.network.net_mac_address,
+            }
+        ),
+        200,
+    )
 
 
-@app.route("/api/info/os/usage")
-def _system_info_os_usage() -> dict[str, str]:
+@app.route("/api/info/os/usage", methods=["GET"])
+def _system_info_os_usage() -> tuple[Response, int]:
     """OS usage info
 
     Request type: GET
@@ -72,12 +85,13 @@ def _system_info_os_usage() -> dict[str, str]:
     Return:
         Information about the usage of the OS running on the server.
     """
+
     logger.api("/api/info/os/usage", logger.request_type.GET)
-    return {"os_uptime": sys_info.software.os.usage.uptime}
+    return jsonify({"os_uptime": sys_info.software.os.usage.uptime}), 200
 
 
-@app.route("/api/info/os/network")
-def _system_info_os_network() -> dict[str, str]:
+@app.route("/api/info/os/network", methods=["GET"])
+def _system_info_os_network() -> tuple[Response, int]:
     """network info
 
     Request type: GET
@@ -87,15 +101,21 @@ def _system_info_os_network() -> dict[str, str]:
     Return:
         Information about servers network.
     """
+
     logger.api("/api/info/os/network", logger.request_type.GET)
-    return {
-        "net_ip": sys_info.software.network.net_ip,
-        "net_mac_address": sys_info.software.network.net_mac_address,
-    }
+    return (
+        jsonify(
+            {
+                "net_ip": sys_info.software.network.net_ip,
+                "net_mac_address": sys_info.software.network.net_mac_address,
+            }
+        ),
+        200,
+    )
 
 
-@app.route("/api/info/hardware")
-def _system_info_hardware() -> dict[str, str]:
+@app.route("/api/info/hardware", methods=["GET"])
+def _system_info_hardware() -> tuple[Response, int]:
     """Hardware info
 
     Request type: GET
@@ -105,19 +125,25 @@ def _system_info_hardware() -> dict[str, str]:
     Return:
         Information about the servers hardware.
     """
+
     logger.api("/api/info/hardware", logger.request_type.GET)
-    return {
-        "os_cpu_arch": sys_info.sys_hardware.cpu.cpu_arch,
-        "os_cpu_cores": sys_info.sys_hardware.cpu.cpu_cores,
-        "os_cpu_core_clock_max": sys_info.sys_hardware.cpu.cpu_core_clock_max,
-        "os_cpu_core_clock_min": sys_info.sys_hardware.cpu.cpu_core_clock_min,
-        "os_cpu_threads": sys_info.sys_hardware.cpu.cpu_threads,
-        "os_hard_ram": sys_info.sys_hardware.ram.size,
-    }
+    return (
+        jsonify(
+            {
+                "os_cpu_arch": sys_info.sys_hardware.cpu.cpu_arch,
+                "os_cpu_cores": sys_info.sys_hardware.cpu.cpu_cores,
+                "os_cpu_core_clock_max": sys_info.sys_hardware.cpu.cpu_core_clock_max,
+                "os_cpu_core_clock_min": sys_info.sys_hardware.cpu.cpu_core_clock_min,
+                "os_cpu_threads": sys_info.sys_hardware.cpu.cpu_threads,
+                "os_hard_ram": sys_info.sys_hardware.ram.size,
+            }
+        ),
+        200,
+    )
 
 
-@app.route("/api/info/hardware/cpu")
-def _system_info_hardware_cpu() -> dict[str, str]:
+@app.route("/api/info/hardware/cpu", methods=["GET"])
+def _system_info_hardware_cpu() -> tuple[Response, int]:
     """CPU info
 
     Request type: GET
@@ -127,18 +153,24 @@ def _system_info_hardware_cpu() -> dict[str, str]:
     Return:
         Information about the servers CPU.
     """
+
     logger.api("/api/info/hardware/cpu", logger.request_type.GET)
-    return {
-        "os_cpu_arch": sys_info.sys_hardware.cpu.cpu_arch,
-        "os_cpu_cores": sys_info.sys_hardware.cpu.cpu_cores,
-        "os_cpu_core_clock_max": sys_info.sys_hardware.cpu.cpu_core_clock_max,
-        "os_cpu_core_clock_min": sys_info.sys_hardware.cpu.cpu_core_clock_min,
-        "os_cpu_threads": sys_info.sys_hardware.cpu.cpu_threads,
-    }
+    return (
+        jsonify(
+            {
+                "os_cpu_arch": sys_info.sys_hardware.cpu.cpu_arch,
+                "os_cpu_cores": sys_info.sys_hardware.cpu.cpu_cores,
+                "os_cpu_core_clock_max": sys_info.sys_hardware.cpu.cpu_core_clock_max,
+                "os_cpu_core_clock_min": sys_info.sys_hardware.cpu.cpu_core_clock_min,
+                "os_cpu_threads": sys_info.sys_hardware.cpu.cpu_threads,
+            }
+        ),
+        200,
+    )
 
 
-@app.route("/api/info/hardware/ram")
-def _system_info_hardware_ram() -> dict[str, str]:
+@app.route("/api/info/hardware/ram", methods=["GET"])
+def _system_info_hardware_ram() -> tuple[Response, int]:
     """RAM info.
 
     Request type: GET
@@ -148,14 +180,20 @@ def _system_info_hardware_ram() -> dict[str, str]:
     Return:
         Information about the servers RAM.
     """
+
     logger.api("/api/info/hardware/ram", logger.request_type.GET)
-    return {
-        "os_hard_ram": sys_info.sys_hardware.ram.size,
-    }
+    return (
+        jsonify(
+            {
+                "os_hard_ram": sys_info.sys_hardware.ram.size,
+            }
+        ),
+        200,
+    )
 
 
-@app.route("/api/info/hardware/cpu/usage")
-def _system_info_hardware_cpu_usage() -> dict[str, str | dict[str, str]]:
+@app.route("/api/info/hardware/cpu/usage", methods=["GET"])
+def _system_info_hardware_cpu_usage() -> tuple[Response, int]:
     """CPU usage info.
 
     Request type: GET
@@ -165,15 +203,21 @@ def _system_info_hardware_cpu_usage() -> dict[str, str | dict[str, str]]:
     Return:
         Information about the servers CPU usage.
     """
+
     logger.api("/api/info/hardware/cpu/usage", logger.request_type.GET)
-    return {
-        "cpu_usage_per_core": sys_info.sys_hardware.cpu.usage.per_core,
-        "cpu_usage_total": sys_info.sys_hardware.cpu.usage.total,
-    }
+    return (
+        jsonify(
+            {
+                "cpu_usage_per_core": sys_info.sys_hardware.cpu.usage.per_core,
+                "cpu_usage_total": sys_info.sys_hardware.cpu.usage.total,
+            }
+        ),
+        200,
+    )
 
 
-@app.route("/api/info/hardware/ram/usage")
-def _system_info_hardware_ram_usage() -> dict[str, str]:
+@app.route("/api/info/hardware/ram/usage", methods=["GET"])
+def _system_info_hardware_ram_usage() -> tuple[Response, int]:
     """RAM usage info.
 
     Request type: GET
@@ -183,15 +227,21 @@ def _system_info_hardware_ram_usage() -> dict[str, str]:
     Return:
         Information about the servers RAM usage.
     """
+
     logger.api("/api/info/hardware/ram/usage", logger.request_type.GET)
-    return {
-        "ram_available": sys_info.sys_hardware.ram.usage.available,
-        "ram_used": sys_info.sys_hardware.ram.usage.used,
-    }
+    return (
+        jsonify(
+            {
+                "ram_available": sys_info.sys_hardware.ram.usage.available,
+                "ram_used": sys_info.sys_hardware.ram.usage.used,
+            }
+        ),
+        200,
+    )
 
 
 @app.errorhandler(404)
-def _error(error) -> dict[str, int]:
+def _error(error) -> tuple[Response, int]:
     """error
 
     Request type: GET
@@ -199,5 +249,6 @@ def _error(error) -> dict[str, int]:
     Return:
         404 error.
     """
+
     logger.api("ERROR: Invalid or malformed API request", logger.request_type.GET)
-    return {"error": 404}
+    return jsonify({"error": "Not Found"}), 404
