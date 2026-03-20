21668aa97985848cce142e9029819a8ca23aecf4
Thomas Wilks <t.wilks@my.bpp.com>
2026-03-20 16:20:18 +0000

    docs: Added and updated documention in Repository

--- server/src/main/java/server/repository/ItemRepository.java
+++ server/src/main/java/server/repository/ItemRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import server.models.Item;
 
+/**
+ * Interface for interacting with the Database.
+ */
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
